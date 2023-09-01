776d724ee538fef5c63c570d6461234f0ab41b5f
Mariana <solisfanny2021@gmail.com>
2023-08-31 23:23:05 -0600

    Mas o menos

--- Parte2.sh
+++ Parte2.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-usuario="fifi"
-grupo="Lab1"
-
-sudo  useradd -m $usuario 2> error.txt
+usuario=$1
+grupo=$2
+useradd -m -p "hola"  $usuario 2> error.txt
 cat error.txt
 
-sudo  groupadd $grupo 2> error2.txt
+groupadd $grupo  2> error2.txt
 
 cat error2.txt
-
-sudo usermod  -aG $grupo $usuario
-sudo usermod  -aG $grupo $USER
-
-chmod 770 $1
+usermod  -a -G $grupo $usuario
+usermod  -a -G $grupo $USER
+chown marisoli:$grupo main.sh
+chmod 770 main.sh
